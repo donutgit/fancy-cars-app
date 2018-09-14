deb6221f56069593fc902d123d477ebb5cfca47a
donutgit <biutifulzika@gmail.com>
2018-09-14 12:55:24 +0300

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import Routes from "./routes";
 import registerServiceWorker from "./registerServiceWorker";
 import { ApolloProvider } from "react-apollo";
 import client from "./apollo/client";
+import ReactGA from "react-ga";
 //css
 import "./index.css";
 
-const app = (
+ReactGA.initialize("UA-125702510-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
+const App = (
   <ApolloProvider client={client}>
     <Routes />
   </ApolloProvider>
 );
 
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(App, document.getElementById("root"));
 registerServiceWorker();
